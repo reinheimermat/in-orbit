0d066d0199043ab1039002afe33d9eafd5bf524d
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-09-09 22:14:02 -0300

    chore(lint): Config Eslint.

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app
-	.listen({
-		port: 3333,
-	})
-	.then(() => {
-		console.log("Server is running on port 3333");
-	});
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
